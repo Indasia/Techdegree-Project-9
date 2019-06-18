db447d17d6be273293e21e3702760f843fc5f462
Indasia Summerfield <Indasia@yahoo.com>
2019-06-18 12:52:14 -0700

    basic structure of models

---
+++ models/User.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => { 
+    const User = sequelize.define('User', {
+
+
+    });
+    
+    return User;
+};
---
+++ models/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+
+
+module.exports = db;
