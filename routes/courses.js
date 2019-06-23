f82e9d0a48127494d0dbc554bc843920ea37b50d
Indasia Summerfield <Indasia@yahoo.com>
2019-06-22 23:50:53 -0700

    working on routes

--- routes/courses.js
+++ routes/courses.js
@@ -2,13 +2,94 @@ const express = require("express");
 const router = require.Router();
 const User = require("../models").User;
 const Course = require("../models").Course;
+const authentication = ("./authentication");
 
 
 // GET - returns list of courses, including the user that owns each course
+router.get('/', function (req, res, next) => {
 
+    // get all the courses
+    Course.findAll({
+        // specific fields
+        attributes: [
+            "id",
+            "title",
+            "description",
+            "estimatedTime",
+            "materialsNeeded",
+            "userId"
+        ],
+        // include User info
+        include: [
+            {
+                model: User,
+                // send required attributes
+                attributes: [
+                    "id",
+                    "firstName",
+                    "lastName",
+                    "emailAddress"
+                ]
+            }
+        ]
+    }).then(courses => {
+        // courses list in response
+        res.json(courses);
+        // set status code to 200
+        res.status(200);
+    //catch error
+    }).catch(error => {
+        error.status = 400;
+        next(error);
+    });
+    
+});
 
 // GET - returns the course, including the user that owns the course, for the provided course ID
+router.get('/:id', function (req, res, next) => {
 
+    const info = req.params;
+    // find specific course
+    Course.findOne({
+        where: {
+            id: info.id;
+        }
+        // specific fields
+        attributes: [
+            "id",
+            "title",
+            "description",
+            "estimatedTime",
+            "materialsNeeded",
+            "userId"
+        ],
+        // include User info
+        include: [
+            {
+                model: User,
+                // send required attributes
+                attributes: [
+                    "id",
+                    "firstName",
+                    "lastName",
+                    "emailAddress"
+                ]
+            }
+        ]
+    }).then(course => {
+        if (course) {
+            // course list in response
+            res.json(course);
+            // set status to 200
+            res.status(200);
+        } else {
+            // send an error
+            const error = new Error("We couldn't find a course using this id");
+            error.status = 400;
+            next(error);
+        }
+    });
+});
 
 // POST - cretes a course, sets the location header to the URI, returns no content
 
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,66 @@
 const express = require("express");
 const router = require.Router();
 const User = require("../models").User;
-
+const authentication = ("./authentication");
+var bcrypt = require('bcryptjs');
 
 
 // GET - returns the currently authenticated user
-
-
+router.get('/', authentication, function (req, res) {
+    res.json({
+        id: req.currentUser.id,
+        firstName: req.currentUser.emailAddress,
+        lastName: req.currentUser.lastName,
+        emailAddress: req.currentUser.emailAddress
+    });
+    res.status(200);
+});
 
 // POST - creates a user, sets the location header to "/", and returns no content
+router.post('/', function (req, res, next) {
 
+    const info = req.body;
+
+    // if email hasn't been entered...
+    if (!info.emailAddress) {
+        // then create an error and set status
+        const error = new Error('You have not entered a valid email address');
+        error.status = 400;
+        next(error);
+        // otherwise...
+    } else {
+        User.findOne({ where: { emailAddress: info.emailAddress } })
+            .then(email => {
+            // if email address already exists...
+            if (email) {
+                const error = new Error('This email address is already in use');
+                error.status = 400;
+                next(error);
+                // if email is valid...
+            } else {
+                // hash password using bcryptjs npm package before persisting user to the database
+                info.password = bcrypt.hashSync(info.password);
+                // create a user
+                User.create(info)
+                    .then(() => {
+                        res.location('/');
+                        res.status(201).end();
+                        // catch error and check if Sequelize validation error
+                    }).catch(error => {
+                        if (error.name === "SequelizeValidationError") {
+                            error.message = "All information must be entered";
+                            error.status = 400;
+                            next(error);
+                        } else {
+                            error.status = 400;
+                            next(error);
+                        }
+
+                    });
+            }
+        });
+    }
+});
 
 
 
